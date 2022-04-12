b6ca872ecbf71b73951ec553f5c10d1a7025a18a
김현승 <corn120033@gmail.com>
2022-04-12 22:10:35 +0900

    Update LifeCycle.cs
    델타타임을 사용하여 프레임이 다른 두 유저 간의 형평성을 맞춘다

--- Assets/LifeCycle.cs
+++ Assets/LifeCycle.cs
@@ -11,9 +11,12 @@ public class LifeCycle : MonoBehaviour
     
     void Update()
     {
+        //델타타임이란 이전 프레임 완료까지 걸린 시간
+        //프레임 값과 반비례하기 때문에 속도나 거리 등의 연산에 사용 시
+        //프레임이 다른 두 유저간의 형평성을 맞출 수 있다
         Vector3 vec = new Vector3(
-            Input.GetAxis("Horizontal"), 
-            Input.GetAxis("Vertical"), 
+            Input.GetAxis("Horizontal") * Time.deltaTime, 
+            Input.GetAxis("Vertical") * Time.deltaTime, 
             0);
         transform.Translate(vec);
     }
