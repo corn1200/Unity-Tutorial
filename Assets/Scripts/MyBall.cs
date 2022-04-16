525fd1f8fdb9a7ca0de51f330ef9a8888f10fd04
김현승 <corn120033@gmail.com>
2022-04-16 11:35:48 +0900

    Update MyBall.cs
    실제 물리적 충돌이 아닌 콜라이더 충돌(겹침)로 인한 이벤트를 지정한다

---
+++ Assets/Scripts/MyBall.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBall : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //힘을 가하기
+        if (Input.GetButtonDown("Jump") && transform.position.y <= transform.lossyScale.y / 2 * 1.1)
+        {
+            rigid.AddForce(Vector3.up * 105, ForceMode.Impulse);
+        }
+
+        Vector3 vec = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            0, Input.GetAxisRaw("Vertical"));
+
+        rigid.AddForce(vec, ForceMode.Impulse);
+
+        //회전력
+        //rigid.AddTorque(Vector3.up);
+    }
+
+    //콜라이더 충돌 지속
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Cube")
+        {
+            rigid.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+    }
+}
